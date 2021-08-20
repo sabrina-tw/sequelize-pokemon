cd04b2e062edcfec9b953bbf64eaf2adf9c52e00
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-20 15:53:44 +0800

    Implement and add test for POST /pokemon/new

--- __tests__/pokemon.route.test.js
+++ __tests__/pokemon.route.test.js
@@ -51,4 +51,32 @@ describe("/pokemon", () => {
       expect(response.body.message).toEqual("done");
     });
   });
+
+  // describe("GET /all", () => {
+  //   it("should return all pokemons", async () => {
+  //     const response = await request(app).get("/all").expect(200);
+
+  //     const { body } = response;
+  //     expect(body.length).toEqual(1);
+  //     expect(body[0]).toMatchObject(pokemon1);
+  //   });
+  // });
+
+  describe("POST /new", () => {
+    it("should create and return the new pokemons", async () => {
+      const requestCreatePokemon1 = JSON.stringify({
+        ...pokemon1,
+        id: null,
+      });
+      const response = await request(app)
+        .post("/new")
+        .send(requestCreatePokemon1);
+
+      expect(response.status).toEqual(200);
+
+      const { body } = response;
+      expect(body.id).toEqual(1);
+      expect(body).toMatchObject(pokemon1);
+    });
+  });
 });
--- routes/pokemon.route.js
+++ routes/pokemon.route.js
@@ -11,6 +11,14 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "done" });
 });
 
+router.post("/new", async (req, res) => {
+  const requestedPokemon = req.body;
+
+  const createdPokemon = await SimplePokemon.create(requestedPokemon);
+
+  res.status(200).json(createdPokemon);
+});
+
 export default () => {
   return router;
 };
