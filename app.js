e0c60e84b365f17837fda9ef81160bab54504f8e
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-22 15:41:35 +0800

    Install and use cookie-parser

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import "dotenv/config";
 import express from "express";
+const app = express();
+
+import cookieParser from "cookie-parser";
+app.use(cookieParser());
 
 import { initPokemonModel } from "./routes/pokemon.route.js";
 import pokemonRouter from "./routes/pokemon.route.js";
 
 import { connectDb } from "./utils/db.js";
 await connectDb();
-
-const app = express();
 
 app.get("/", async (req, res) => {
   console.log("Path / is hit");
