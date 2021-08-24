962bc2e53ddcb84abd9bf82c920bf159225e65ee
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-24 09:57:45 +0800

    Update create-trainers migration file to match Trainer model

--- migrations/20210824014656-create-trainer.js
+++ migrations/20210824014656-create-trainer.js
@@ -1,30 +1,33 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Trainers', {
+    await queryInterface.createTable("Trainers", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Trainers');
-  }
+    await queryInterface.dropTable("Trainers");
+  },
 };
