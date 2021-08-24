3b5314e00690a7cd3b022c1bf0ed57dea3c8b367
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-24 11:11:27 +0800

    Add protected GET /trainers/:username route

--- routes/trainers.js
+++ routes/trainers.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { protectRoute } = require("../middleware/protectRoute");
 const db = require("../models/index");
 
 const router = express.Router();
@@ -7,6 +8,19 @@ router.post("/", async (req, res) => {
   try {
     const newTrainer = await db.Trainer.create(req.body);
     res.send(newTrainer);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
+});
+
+router.get("/:username", protectRoute, async (req, res, next) => {
+  try {
+    const username = req.params.username;
+    const trainer = await db.Trainer.findOne({
+      where: { username: { [db.Sequelize.Op.iLike]: "%" + username + "%" } },
+    });
+    res.send(trainer);
   } catch (err) {
     console.error(err);
     res.sendStatus(400);
