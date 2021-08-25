629cd575ad9a6b5bd8285ebf2469358b6e42f0d8
Yu Lian Ng <yulian.ng@thoughtworks.com>
2021-08-25 19:37:53 +0800

    fix feeding in of dialectOptions

--- config/config.js
+++ config/config.js
@@ -21,9 +21,21 @@ module.exports = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     dialect: "postgres",
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    // logging: console.log,                  // Default, displays the first parameter of the log function call
+    // logging: (...msg) => console.log(msg), // Displays all log function call parameters
+    // logging: false,                        // Disables logging
+    pool: {
+      max: 10, // default: 5
+      min: 0, // default: 0
+      idle: 10000, // default: 10000ms
+      acquire: 30000, // default: 60000ms
+      evict: 1000 // default: 1000ms
     }
   },
 };
