63cce246b5523f38287a419c29f8e099642412e4
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-25 11:49:13 +0800

    Add associations for pokemon/trainer models

---
+++ models/pokemon.model.js
@@ -0,0 +1,42 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Pokemon extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Pokemon.hasOne(models.Trainer, { foreignKey: "id" });
+    }
+  }
+  Pokemon.init(
+    {
+      name: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+      },
+      japaneseName: {
+        type: DataTypes.STRING,
+      },
+      baseHP: {
+        type: DataTypes.INTEGER,
+      },
+      category: {
+        type: DataTypes.STRING,
+      },
+      trainerId: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Pokemon",
+    }
+  );
+  return Pokemon;
+};
